3d176914aa7436737a1b6cdcf1adf818b008611a
Maksim N <ufesapresto@gmail.com>
2023-02-12 21:24:22 +0300

    Result Task04

---
+++ Task 04/Program.cs
@@ -0,0 +1,17 @@
+﻿// // Программа, которая
+// принимает на вход три числа
+// и выдает максимальное из этих чисел
+
+Console.WriteLine("Введите число 1");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB>max) max=numberB;
+if (numberC>max) max=numberC;
+Console.WriteLine($"Максимальное число {max}");
+
+
+
