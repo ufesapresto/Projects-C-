63f6a5b064009cc630037e6506e99a62ecd067e7
Maksim N <ufesapresto@gmail.com>
2023-02-12 21:46:05 +0300

    Result Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,14 @@
+﻿// // Программа, которая
+// на вход принимает число
+// и выдает, является ли число четным
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0) 
+{
+Console.WriteLine($"Число {numberA} является четным");
+}
+else
+{
+Console.WriteLine($"Число {numberA} не является четным");
+}
