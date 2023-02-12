a857bc9387919631bd54aa6784c22bf1afb72367
Maksim N <ufesapresto@gmail.com>
2023-02-12 22:50:19 +0300

    Result Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// // Программа, которая
+// на вход принимает число N
+// и на выходе выдает, все четные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= number)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write($"{index} ");
+    }
+    index++;
+}
