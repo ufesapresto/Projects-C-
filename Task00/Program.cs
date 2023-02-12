a16d0831fedb6711352ce7ada9fe2793cd311969
Maksim N <ufesapresto@gmail.com>
2023-02-12 20:48:49 +0300

    added Task02

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая на вход принимает число и выдает его квадрат
+
+Console.WriteLine("Введите число 1");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA / numberB == 
