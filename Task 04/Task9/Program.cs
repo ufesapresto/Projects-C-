24fd552789cde0e6a4dae4e34c1d1697a0c34a3b
Maksim N <ufesapresto@gmail.com>
2023-02-14 20:55:11 +0300

    Added Task9

---
+++ Task 04/Task9/Program.cs
@@ -0,0 +1,27 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit)
+//     Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// else
+//     Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    
+    // if (firstDigit > secondDigit) return firstDigit;
+    // return secondDigit;
+    
+    // int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    // return result;
+
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
