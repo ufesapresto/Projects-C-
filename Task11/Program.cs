ad4ccee82f417fd9a023cac9aaa1e0a65f33ecf4
Maksim N <ufesapresto@gmail.com>
2023-02-16 20:20:53 +0300

    Added Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {number}");
+
+int digit = Digit (number);
+Console.WriteLine($"Answer {digit}");
+
+int Digit(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+return firstDigit * 10 + secondDigit;
+}
+
