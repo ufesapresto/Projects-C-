53592d104bb4375f7bb9c85bcf4916ea9c533877
Maksim N <ufesapresto@gmail.com>
2023-02-18 08:01:52 +0300

    Added Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+bool Square(int num1, int num2)
+{
+	return num1 * num1 == num2 || num2 * num2 == num1;
+}
+if (Square(numberOne, numberTwo)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
