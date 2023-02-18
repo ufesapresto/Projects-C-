8dc06b5f52c47e427a2caa5d715dac516c7315a2
Maksim N <ufesapresto@gmail.com>
2023-02-18 08:44:15 +0300

    Added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 10: Напишите программу, которая 
+//принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите любое трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int digit = Digit(number);
+    Console.WriteLine($"Answer {digit}");
+
+    int Digit(int num)
+    {
+        int firstDigit = num % 100;
+        int secondDigit = num % 10;
+        return (firstDigit - secondDigit) / 10;
+    }
+
+}
+else Console.WriteLine("Вы ввели неверное число");
