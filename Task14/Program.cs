852012598617c6c15b3cbc62e118f6d8c952ec5a
Maksim N <ufesapresto@gmail.com>
2023-02-18 07:54:26 +0300

    Added Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+bool Multiplicity(int num1)
+{
+	return num1 % (7*23) == 0;
+}
+if (Multiplicity(numberOne)) Console.WriteLine("Введенное число кратно 7 и 23");
+else Console.WriteLine("Нет");
