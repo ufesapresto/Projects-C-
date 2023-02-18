45b6527898cdb9875917f7f82b48c4ba6a32a5b4
Maksim N <ufesapresto@gmail.com>
2023-02-18 07:44:18 +0300

    Added Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int Add = numberOne % numberTwo;
+bool Square(int num1, int num2)
+{
+	return num1 * num1 == num2 || num2 * num2 == num1;
+}
+if (Square(numberOne, numberTwo)) Console.WriteLine("Одно число является квадратом второго");
+else Console.WriteLine(Add);
+
+
+
+//Console.WriteLine("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (Multiplicity(number)) Console.WriteLine("Кратно");
+//else Console.WriteLine("Не кратно");
+//
+//bool Multiplicity(int num)
+//{
+//	int multCheck1 = num % 7;
+//	int multCheck2 = num % 23;
+//	return multCheck1 == 0 && multCheck2 == 0;
+//}
