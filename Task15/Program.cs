667dfcb1d87d9f8a8c5e51c830178f2ab53af0fe
Maksim N <ufesapresto@gmail.com>
2023-02-19 19:37:26 +0300

    Added Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 15: Напишите программу, которая 
+//принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите любое число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 1 && number <= 7)
+{
+    bool Holiday(int num)
+    {
+        return num > 5 && num < 8;
+    }
+    if (Holiday(number)) Console.WriteLine($"Да");
+    else Console.WriteLine($"Нет");
+}
+else Console.WriteLine("Вы ввели неверное число");
+
+
