418b39cb1a63f172323b61209a43f70434c0cd51
Maksim N <ufesapresto@gmail.com>
2023-02-22 07:05:04 +0300

    Added Task18

---
+++ Task 18/Program.cs
@@ -0,0 +1,28 @@
+﻿//Программа, которая по номеру четверти
+//сообщает диапазон координат
+
+//методы
+int Printline(string massage)
+{
+    Console.WriteLine(massage);
+    int num = Convert.ToInt32(Console.ReadLine()); 
+    return num;   
+}
+
+string Range (int num)
+{
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x < 0, y > 0";
+    if (num == 3) return "x < 0, y < 0";
+    if (num == 4) return "x > 0, y < 0";
+    return "0";
+}
+
+//тело программы
+int number = Printline("Введите номер четверти");
+string range = Range(number);
+string result = range == "0"
+                    ?"Ошибка ввода"
+                    : $"Диапазон возможных координат точек в этой четверти {range}";
+
+Console.WriteLine(result);
