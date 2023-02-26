cf0c82116c45d66c7bf2a01e3935760f9a072842
Maksim N <ufesapresto@gmail.com>
2023-02-26 14:02:33 +0300

    Added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,34 @@
+﻿// Программа, которая на ввод принимает любое число N
+//и выдаёт произведение чисел от 1 до N
+
+//методы
+///метод ввода данных
+int printLine(string massage)
+{
+    Console.WriteLine(massage);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int summ(int number)
+{
+    int s = 1;
+    for (int i = 1; i <= number; i++)
+    {
+       checked
+       {
+        s = s * i;
+       }    
+    
+    }
+    return s;
+    //while (i <= number)
+    //{
+      //  s = s * (i);
+      //  i++;
+    //}
+    //
+}
+int n = printLine("Введите любое число");
+int result = summ(n);
+Console.WriteLine($"Произведение чисел от 1 до {n} -> {result}");
