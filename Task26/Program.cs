3d9a5b05f0684e48ba93d5dc81abaf7868fe6b2c
Maksim N <ufesapresto@gmail.com>
2023-02-26 13:30:00 +0300

    Added Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая на ввод принимает любое число N
+//и выдаёт количество цифр в числе
+
+//методы
+///метод ввода данных
+int printLine(string massage)
+{
+    Console.WriteLine(massage);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int lenthNumber(int number)
+{
+    int lenth = 0;
+    while (number != 0)
+    {
+        lenth = lenth + 1;
+        number = number / 10;
+
+    }
+    return lenth;
+}
+int n = printLine("Введите любое число");
+int l = lenthNumber(n);
+Console.WriteLine($"Количество знаков в числе {n} -> {l}");
