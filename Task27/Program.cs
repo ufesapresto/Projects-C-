8b63e9e9a69cf320094ee81763e8581548999c79
Maksim N <ufesapresto@gmail.com>
2023-02-26 18:14:19 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+//медод ввода данных
+int Printline(string massage)
+{
+    Console.WriteLine(massage);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int number = Printline("Введите любое число");
+
+//метод рассчета
+
+int SummNumber(int a)
+{
+    int summ = 0;
+    while (a > 0)
+    {
+        summ = summ + a % 10;
+        a = a / 10;
+    }
+    return summ;
+}
+
+int answer = SummNumber(number);
+Console.WriteLine($"Сумма цифр числа {number} -> {answer}");
