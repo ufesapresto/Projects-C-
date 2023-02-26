6ac9877fbf5ff8209bedc6d4cc55f2bb66bbca68
Maksim N <ufesapresto@gmail.com>
2023-02-26 17:06:21 +0300

    Added Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите длину массива");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = newArray(num);
+Console.WriteLine($"Полученный массив из {num} элементов");
+printArray(arr);
+
+int[] newArray(int num) 
+{
+    int[] array = new int[num];
+    Random ran = new Random();
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = ran.Next(0,2);
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
