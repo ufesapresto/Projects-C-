5b090376895a49f48adab4afac650686c6415309
Maksim N <ufesapresto@gmail.com>
2023-03-01 07:16:06 +0300

    Added Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,40 @@
+﻿// Программа, которая создает массив из 123 чисел и 
+// определяет количество элементов массива в диапазоне от 10 до 99
+
+int[] CreateNewRndInt (int size, int min, int max)
+{
+    int [] array = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+// метод вывода массива в консоль
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1)Console.Write(",");
+    }
+    Console.WriteLine("]");
+} 
+//медод поиска количества чисел
+int QuantityNumbers(int [] array)
+{
+    int count = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i] >= 10 && array[i] <= 99) count++;
+    }
+return count;
+} 
+
+
+int[] arr = CreateNewRndInt(123, 0, 1000);
+PrintArray(arr);
+int quantity = QuantityNumbers(arr);
+Console.WriteLine(quantity);  
