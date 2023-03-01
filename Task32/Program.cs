2e0f499a68bf5610472571a3bc47afbf218bdd4b
Maksim N <ufesapresto@gmail.com>
2023-03-01 06:13:06 +0300

    Added Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,38 @@
+﻿//// Задача 32: Напишите программу замены элементов массива
+// которая заменяет положительные элементы на соответствующие отрицательные, и наоборот
+
+//метод создания массива
+int[] CreateNewRndInt (int size, int min, int max)
+{
+    int [] array = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+// метод вывода массива в консоль
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1)Console.Write(",");
+    }
+    Console.WriteLine("]");
+} 
+//медод инверсии
+void CangedArray(int [] array)
+{
+for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = array [i] * -1;
+    }
+} 
+int[] arr = CreateNewRndInt(12, -100, 100);
+PrintArray(arr);
+Console.WriteLine();
+CangedArray(arr);
+PrintArray(arr);
