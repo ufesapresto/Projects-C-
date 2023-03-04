4f30f2287d4a9f09510580ee02473d2e961c3550
Maksim N <ufesapresto@gmail.com>
2023-03-04 12:18:29 +0300

    Added Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая принимает на вход 3 числа и проверяет
+// может ли существовать треугольник с такими длинами сторон
+
+//метод ввода чисел
+int Printline(string massage)
+{
+    Console.WriteLine(massage);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+//метод проверки
+
+bool TriangleExist(int a, int b, int c)
+{
+return(a < b + c && b < c + a && c < a + b);
+}
+
+int firstNumber = Printline("Введите первое число");
+int secondNumber = Printline("Введите второе число");
+int thirdNumber = Printline ("Введите третье число");
+if(TriangleExist(firstNumber, secondNumber, thirdNumber))
+Console.WriteLine("Треугольник с такими длинами сторон существует");
+else Console.WriteLine("Треугольник с такими длинами сторон не существует");
