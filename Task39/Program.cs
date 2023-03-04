e27effb9e597d918b2fb90d33db68dd02058d417
Maksim N <ufesapresto@gmail.com>
2023-03-04 11:39:10 +0300

    Added Task39

---
+++ Task39/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 39: Напишите программу, которая разворачивает массив из 12 элементов
+
+//метод создания массива
+int[] CreateNewRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+//метод вывода массива в консоль
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.Write("]");
+}
+
+// метод разворота массива
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+int[] arr = CreateNewRndInt(9, 0, 9);
+PrintArray(arr);
+Console.Write(" -> ");
+ReverseArray(arr);
+PrintArray(arr);
+Console.WriteLine();
