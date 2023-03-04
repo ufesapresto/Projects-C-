05bba9ca16a8f563df65db5bc63bb2c4a2d57b10
Maksim N <ufesapresto@gmail.com>
2023-03-04 20:08:36 +0300

    Added Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа преобразования десятичного числа в двоичное
+
+//метод ввода двоичного числа
+int Printline(string massage)
+{
+    Console.WriteLine(massage);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int number = Printline("Введите число");
+// 45/2= 22 (1), 22/2= 11 (0), 11/2= 5 (1), 5/2= 2 (1), 2/2= 1 (0), 1/2= 0 (1)-> 101101
+
+int NewNumber(int num)
+{
+    int newNumber = 1;
+    int rez = 0;
+while (num > 0)
+{    
+    rez = rez + num % 2 * newNumber;
+    num = num / 2;
+    newNumber *= 10;
+}
+return rez;
+}
+
+int a = NewNumber(number);
+Console.WriteLine(a);
+
+
+//int beNumber = Console.ReadLine()* 10 + 1;
+
+//while (beNumber / 10 > 0)
+//{
+//    int answer = beNumber % 10;
+//    (answer);
+//    beNumber = beNumber / 10;
+//}
