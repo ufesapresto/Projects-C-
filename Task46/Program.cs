43dbdf41d70cfdc207adc18a43d81a2ca46a8fbd
Maksim N <ufesapresto@gmail.com>
2023-03-07 07:13:22 +0300

    Added Task46

---
+++ Task46/Program.cs
@@ -0,0 +1,34 @@
+﻿// Программа создания массива, размером m,n
+// заполненного случайными числами
+
+//метод создания двумерного массива
+int[,] CreateNewMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+//метод вывода массива в консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 3}, ");
+            else Console.Write($"{matrix[i, j], 3}"); ;
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2d = CreateNewMatrix(3, 4, -99, 99);
+PrintMatrix(array2d);
