cac263c9128393b2b46d98fe1ff7d4ac47ce9aee
Maksim N <ufesapresto@gmail.com>
2023-03-11 09:31:20 +0300

    Added Task48

---
+++ Task48/Program.cs
@@ -0,0 +1,33 @@
+﻿// Программа создания массива, размером m,n
+// заполненного числами по формуле Amn = m + n
+
+//метод создания двумерного массива
+int[,] CreateNewMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+//метод вывода массива в консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 3}, ");
+            else Console.Write($"{matrix[i, j], 3}"); ;
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2d = CreateNewMatrix(3, 4);
+PrintMatrix(array2d);
