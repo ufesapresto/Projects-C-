f921aa56951400d0b4372f9b63cbd962a37ffbf5
Maksim N <ufesapresto@gmail.com>
2023-03-12 13:36:13 +0300

    Added Task52

---
+++ Task 52/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+//метод создания двумерного массива
+int[,] CreateNewMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+//метод вывода массива в консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3}"); ;
+        }
+        Console.WriteLine("]");
+    }
+}
+
+// метод вывода массива округленных чисел в консоль
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{Math.Round(array[i], 1)}");
+        if (i < array.Length - 1) Console.Write(";  ");
+    }
+    Console.WriteLine();
+}
+
+// метод создания массива из значений, равных среднему арифметическому столбца
+double[] AverageArray(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(1)];
+    for (int j = 0; j < array.Length; j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        array[j] = sum / matrix.GetLength(0);
+    }
+    return array;
+}
+
+
+int[,] array2d = CreateNewMatrix(5, 5, -99, 99);
+PrintMatrix(array2d);
+double[] result = AverageArray(array2d);
+Console.WriteLine("Средние арифмечисеские элементов в каждом столбце");
+PrintArray(result);
