d88666bf3c429fdea069b5fcfac209685817b4ce
Maksim N <ufesapresto@gmail.com>
2023-03-13 22:10:14 +0300

    Added Task59

---
+++ Task 59/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 59. Задайте двумерный массив. Напишите программу, 
+//которая удаляет строку и столбец, на пересечении которых находится 
+//наименьший элемент заданного массива
+
+int[,] CreateNewMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+//метод вывода массива в консоль
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3}"); ;
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] IndexMinimalElement(int[,] matrix)
+{
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] <= matrix[row, column])
+            {
+                row = i;
+                column = j;
+            }
+        }
+    }
+    return new int[] { row, column };
+}
+
+int[,] DeliteRowColumnMinElem(int[,] matrix, int[] array)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    int m = 0, n = 0;
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        if (m == array[0]) m++;
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            if (n == array[1]) n++;
+            newMatrix[i, j] = matrix[m, n];
+            n++;
+        }
+        m++;
+        n = 0;
+    }
+    return newMatrix;
+}
+
+int[,] firstMatrix = CreateNewMatrix(5, 5, 0, 9);
+PrintMatrix(firstMatrix);
+int[] array = IndexMinimalElement(firstMatrix);
+int[,] resultMatrix = DeliteRowColumnMinElem(firstMatrix, array);
+PrintMatrix(resultMatrix);
