4a60b91340bb1d3077ebb539fa6c0c8a10c31ed3
Maksim N <ufesapresto@gmail.com>
2023-03-22 22:03:48 +0300

    Added Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа, которая считает сумму чисел в числе
+
+Console.WriteLine ("Введите любое натуральное число");
+int number = int.Parse(Console.ReadLine());
+
+//SumNumbers(number);
+
+int SumNumbers(int num)
+{
+    if(num == 0) return 0;
+    else return num % 10 + SumNumbers(num / 10);
+}
+Console.WriteLine($"{SumNumbers(number)}");
