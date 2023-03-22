def339b6b927661c5003ff02b2ef886db8d6b510
Maksim N <ufesapresto@gmail.com>
2023-03-22 22:46:43 +0300

    Added Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на ввод 2 числа и возводит первое в степень второго
+Console.WriteLine ("Введите любое натуральное число");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("Введите любое натуральное число");
+int number2 = int.Parse(Console.ReadLine());
+
+int DigitNumbers(int num1, int num2)
+{
+ if(num2 == 0) return 1;
+ else return num1 * DigitNumbers(num1, num2-1);
+}
+Console.WriteLine($"{DigitNumbers(number1, number2)}");
