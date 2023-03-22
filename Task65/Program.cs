2db0c03643ccbed9781df447ae6b8581c96d3ec3
Maksim N <ufesapresto@gmail.com>
2023-03-22 07:19:56 +0300

    Added Task65

---
+++ Task65/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает числа M, N, 
+// и выводит все натуральные числа от M до N
+Console.WriteLine ("Введите любое натуральное число");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("Введите любое натуральное число");
+int numberN = int.Parse(Console.ReadLine());
+if(numberM < numberN)
+{
+NaturalNumbersFromMtoN(numberM, numberN);
+
+void NaturalNumbersFromMtoN(int numM, int numN)
+{
+    if(numN < numM) return;
+    NaturalNumbersFromMtoN(numM, numN - 1);
+    Console.Write($"{numN} ");
+    
+}
+}
+else Console.WriteLine ("Первое число должно быть меньше второго");
