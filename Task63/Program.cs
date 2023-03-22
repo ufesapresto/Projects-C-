11558933654cf87db13dec81488c5937f589d699
Maksim N <ufesapresto@gmail.com>
2023-03-22 06:53:12 +0300

    Added Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,14 @@
+﻿// Рекурсия. Программа вывода чисел от 1 до N
+
+Console.WriteLine ("Введите любое натуральное число");
+int number = int.Parse(Console.ReadLine());
+
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+    
+}
